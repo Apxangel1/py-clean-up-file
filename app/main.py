742b70ef8fd43d65f06ba69326dd0b380dca7375
Volodymyr Vitsenko <your@mail.com>
2025-03-08 16:38:25 +0100

    cleaner

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
+from typing import Any
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, file_name: str) -> None:
+        self.filename = file_name
+        self.file = None
+
+    def __enter__(self) -> Any:
+        return self
+
+    def __exit__(self) -> None:
+        os.remove(self.filename)
